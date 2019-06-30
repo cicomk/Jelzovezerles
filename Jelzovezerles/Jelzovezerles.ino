bfe47994e742b6fdd1a3b56d32c7c6bbebae4091
Molnar Balazs <cicomk.balazs@outlook.com>
2019-06-30 15:14:01 +0200

    Add README

---
+++ Jelzovezerles/Jelzovezerles.ino
@@ -0,0 +1,50 @@
+//Out
+int out1 = 0;
+int out2 = 1;
+int out3 = 2;
+int out4 = 3;
+int out5 = 4;
+int out6 = 5;
+int out7 = 6;
+int out8 = 7;
+int out9 = 8;
+int out10 = 9;
+int out11 = 10;
+int out12 = 11;
+int out13 = 12;
+int out14 = 13;
+
+//In
+int in1 = A0;
+int in2 = A1;
+int in3 = A2;
+int in4 = A3;
+int in5 = A4;
+
+void setup() {
+  pinMode(out1, OUTPUT);
+  pinMode(out2, OUTPUT);
+  pinMode(out3, OUTPUT);
+  pinMode(out4, OUTPUT);
+  pinMode(out5, OUTPUT);
+  pinMode(out6, OUTPUT);
+  pinMode(out7, OUTPUT);
+  pinMode(out8, OUTPUT);
+  pinMode(out9, OUTPUT);
+  pinMode(out10, OUTPUT);
+  pinMode(out11, OUTPUT);
+  pinMode(out12, OUTPUT);
+  pinMode(out13, OUTPUT);
+  pinMode(out14, OUTPUT);
+
+}
+
+void loop() {
+  tolatasjelzo();
+
+}
+
+void tolatasjelzo() {
+
+
+}
